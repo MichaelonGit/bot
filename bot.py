fb3a2c19208ed6c73e8676ce35e480273a421ab4
MichaelonGit <113832072+MichaelonGit@users.noreply.github.com>
2023-02-05 16:51:54 +0100

    Update bot.py
    merged mod.py with bot.py

--- bot.py
+++ bot.py
@@ -2,74 +2,95 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 import os
-
-bot = commands.Bot(command_prefix='!',intents = discord.Intents.all())
-arg=1
+import datetime
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+arg = 1
 channel = bot.get_channel(1071416735909036154)
+
+
 @bot.event
 async def on_ready():
-    id = 'your id here please delet this you only need the number without apostrophe'
-    channel = bot.get_channel(id)
-    activity = discord.Activity(name='Minecraft', type=discord.ActivityType.playing,large_image='lol', large_text='Yes')
+    channel = bot.get_channel(1071757524941951037)
+    activity = discord.Activity(
+        name='Minecraft', type=discord.ActivityType.playing, large_image='lol', large_text='Yes')
     await bot.change_presence(activity=activity)
     await channel.send('Bot online')
+
+
 async def on_error():
-    error()
+    channel = bot.get_channel(1071757524941951037)
+    await channel.send('error')
+
+
+async def on_guild_join(guild):
+    channel = bot.get_channel(1071757524941951037)
+    await channel.send("Wassup! welcome ")
+
+
+@bot.listen('on_message')
+async def lol(message):
+    """ some on_message command """
+    if message.author.id == bot.user.id:
+        return
+    msg_content = message.content.lower()
+
+    curseWord = ['fuck you', 'bitch', 'motherfucker',
+                 'noob', 'not verry smart', 'go and fuck yourself']
+
+    # delete curse word if match with the list
+    if any(word in msg_content for word in curseWord):
+        await message.delete()
+        member = message.author
+        await member.send(f"{member.mention}"'you have been muted for badword')
+        await member.timeout(datetime.timedelta(minutes=10))
+
+        await message.channel.send(f"{member.mention}"'has been muted! for bad word!')
+
+
 @bot.command()
 async def website(ctx):
     await ctx.channel.send('https://github.com/MichaelonGit/bot')
+
+
 @bot.command()
-async def unmute(ctx,member: discord.Member):
+async def mute(ctx, member: discord.Member,minutes:int):
+    member1 = ctx.message.author
     if ctx.author.top_role.permissions.administrator == True:
-        role = member.guild.get_role(1069634546804461608)
-        member1 = ctx.message.author
-        await member.remove_roles(role)
-        await ctx.channel.send(f"{member1.mention}"'has unmuted'f"{member.mention}")
+        if minutes >= 1:
+            member1 = ctx.message.author
+            await member.timeout(datetime.timedelta(minutes=minutes))
+            print('yes', member)
+            yes = minutes
+            await ctx.channel.send(f"{member1.mention}"'has muted'f"{member.mention}" )
+        else:
+            await ctx.reply(member1.mention + ' has removed some minutes ' + ' from ' + member.mention)
     else:
         await ctx.channel.send('404 Permission not found :(')
+
+
 @bot.command()
-async def mute(ctx,member: discord.Member):
-    if ctx.author.top_role.permissions.administrator == True:
-        member1 = ctx.message.author
-        role = member.guild.get_role(1069634546804461608)
-        role1 = member.guild.get_role(1069168060168159262)
-        await member.add_roles(role)
-        await member.remove_roles(role1)
-        print('yes',member)
-        await ctx.channel.send(f"{member1.mention}"'has muted'f"{member.mention}")
-        
-    else:
-        await ctx.channel.send('404 Permission not found :(')
-@bot.command()
-async def minserverip(ctx):
-    member= ctx.message.author
-    hostname = 'https://github.com/MichaelonGit/bot'
-    response = os.system("ping -c 1 " + hostname + ' 2>/dev/null 1>&2')
-    if response == 0:
-        await ctx.send(member.mention + " " + hostname + ' is currently online')
-    else:
-        await ctx.send(member.mention + " " + hostname + ' is down')
-    await ctx.channel.send('https://github.com/MichaelonGit/bot')
-    await ctx.send(f"{member.mention}")
-@bot.command()
-async def list(ctx,message):
+async def list(ctx, message):
     if ctx.author.top_role.permissions.administrator == True:
         if message == 'admin':
             member = ctx.message.author
             yes = f"{member.mention}"
-            sus ='admin commands : `!list admin` and `!mute <user with tag>`'
+            sus = 'admin commands : `!list admin` and `!mute <user with tag>`'
             await member.send(yes)
             await member.send(sus)
-            await ctx.send(f"{member.mention}"'look in your dms!')
-            
+            await ctx.reply(f"{member.mention}"'look in your dms!')
+            await ctx.message.delete()
         else:
-            embed=discord.Embed(title="Help:", url="https://sites.google.com/view/the-bot/startseite", description="Evry command is here", color=0xFF5733)
-            await ctx.send(embed=embed)
+            embed = discord.Embed(title="Help:", url="https://github.com/MichaelonGit/bot",
+                                  description="Evry command is here", color=0xFF5733)
+            await ctx.reply(embed=embed)
     else:
-        embed=discord.Embed(title="Help:", url="https://sites.google.com/view/the-bot/startseite", description="Evry command is here", color=0xFF5733)
-        await ctx.send(embed=embed)
+        embed = discord.Embed(title="Help:", url="https://github.com/MichaelonGit/bot",
+                              description="Evry command is here", color=0xFF5733)
+        await ctx.reply(embed=embed)
+
+
 @bot.command()
-async def ping(ctx,arg):
+async def ping(ctx, arg):
     hostname = arg
     member = ctx.message.author
     response = os.system("ping -c 1 " + hostname + ' 2>/dev/null 1>&2')
@@ -77,8 +98,44 @@ async def ping(ctx,arg):
         await ctx.send(member.mention + " " + hostname + ' is up')
     else:
         await ctx.send(member.mention + " " + hostname + ' is down')
+
+
 @bot.command()
 async def error(ctx):
     await ctx.send('somthing went wrong||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|| _ _ _ _ _ _https://myfilesigusse.tixte.co/error.mp4')
 
-bot.run('Token insert here')
+
+@bot.command()
+async def github(ctx):
+    embed = discord.Embed(title="My github page:", url="https://github.com/MichaelonGit/bot",
+                          description="My gitub is not new lol", color=0xce0d0d)
+    embed.set_author(name="Michael9Gamer", url="https://github.com/MichaelonGit/bot",
+                     icon_url="https://myfilesigusse.tixte.co/linux.png")
+    embed.set_thumbnail(url="https://myfilesigusse.tixte.co/linux.png")
+    embed.set_footer(text="lol")
+    await ctx.reply(embed=embed)
+
+
+@bot.command()
+async def mail(ctx, member: discord.Member, *, msg):
+    member1 = ctx.message.author
+    await ctx.message.delete()
+    await member.send(msg + "" + ' | ' + f"{member1.mention}" + ' send you a message')
+
+
+@bot.command()
+async def kick1(ctx, member: discord.Member):
+    admin = member.guild.get_member(831904772721868900)
+    if ctx.author.top_role.permissions.administrator == True:
+        await member.kick()
+    else:
+        await ctx.reply(member.mention + 'you are trying to kick?' + admin.mention)
+
+
+@bot.command()
+async def support(ctx):
+    await ctx.reply('https://discord.gg/SWVwQqZ4vK')
+@bot.command()
+async def nevergonnagiveyouup(ctx):
+    await ctx.reply('Never gonna give you up!||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|| _ _ _ _ _ _https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+bot.run('token')
